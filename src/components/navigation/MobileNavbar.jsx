1eaacb53618d80b9978ef883193afe887c28d0e7
bbautista73 <bbautista73@gmail.com>
2023-04-12 12:02:26 -0400

    init commit

---
+++ src/components/navigation/MobileNavbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './MobileNavbar.scss';
+
+const MobileNavbar = () => {
+    return (
+        <div className="mobile-navbar">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+        </div>
+    );
+}
+
+export default MobileNavbar;
