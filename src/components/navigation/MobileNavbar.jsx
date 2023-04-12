6f46f61b7eb86b5b4f334d782517abd11f94b660
bbautista73 <bbautista73@gmail.com>
2023-04-12 19:12:29 -0400

    added local server

--- src/components/navigation/MobileNavbar.jsx
+++ src/components/navigation/MobileNavbar.jsx
@@ -4,9 +4,26 @@ import './MobileNavbar.scss';
 const MobileNavbar = () => {
     return (
         <div className="mobile-navbar">
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#contact">Contact</a>
+            <div className="mobile-navbar-button">
+                <i className="las la-th-large mobile-navbar-button-icon"></i>
+                <a className="mobile-navbar-button-link" href="#home">Routines</a>
+            </div>
+            <div className="mobile-navbar-button">
+                <i className="las la-history"></i>
+                <a className="mobile-navbar-button-link" href="#about">History</a>
+            </div>
+            <div className="mobile-navbar-button">
+                <i className="las la-chart-area"></i>
+                <a className="mobile-navbar-button-link" href="#stats">Stats</a>
+            </div>
+            <div className="mobile-navbar-button">
+                <i className="las la-dumbbell"></i>
+                <a className="mobile-navbar-button-link" href="#exercises">Exercises</a>
+            </div>
+            <div className="mobile-navbar-button">
+                <i className="las la-cog"></i>
+                <a className="mobile-navbar-button-link" href="#settings">Settings</a>
+            </div>
         </div>
     );
 }
